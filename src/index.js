1c7e5cb9e61fef64f824916c7aea26853b569c30
genie <soccer910@naver.com>
2024-12-02 13:12:42 +0900

    newadd

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import testModule from "./test.js";
+
+console.log(new testModule("홍길동"));
---
+++ src/test.js
@@ -0,0 +1,7 @@
+function User(name) {
+  this.name = name;
+}
+
+export default User;
+
+//default 는 const와 let과 var와는 함계 사용 X
