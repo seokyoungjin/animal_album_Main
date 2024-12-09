ad3087671cf8b45ea0dc626aee1c0d4983a2e387
genie <soccer910@naver.com>
2024-12-09 21:01:08 +0900

    ok

--- animal_album-3/server/server.js
+++ animal_album-3/server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.joi(__dirname, "..")));
+app.use(express.static(path.join(__dirname, "..")));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "index.html"));
